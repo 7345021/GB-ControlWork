d5270252665169131b828ac0972fce83c1832ef4
Andrey <trusov1967@gmail.com>
2022-03-16 20:48:02 +0300

    Вывели на печать изначальный массив и ввели переменную, обозначающую количество символов

--- work1/Program.cs
+++ work1/Program.cs
@@ -2,3 +2,15 @@
 // длина которых меньше, либо равна 3 символа. Первоначальный массив можно задать либо с клавиатуры,
 //либо задать на старте выполнения алгоритма.
 string[] myString = new string[] { "hello", "two", "C#", "we", "one", "nine", "and", "first" };
+Console.WriteLine("Первоначальный массив");
+Console.WriteLine(myString[0]);
+Console.WriteLine(myString[1]);
+Console.WriteLine(myString[2]);
+Console.WriteLine(myString[3]);
+Console.WriteLine(myString[4]);
+Console.WriteLine(myString[5]);
+Console.WriteLine(myString[6]);
+Console.WriteLine(myString[7]);
+Console.WriteLine();
+int L=3;
+Console.WriteLine("Элементы массива с количеством символов <=3: ");
