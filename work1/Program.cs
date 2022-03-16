81ea710c4d25b1dfe7a62439ab9f12dd93da050e
Andrey <trusov1967@gmail.com>
2022-03-16 21:01:47 +0300

    Решил задачу вторым способом(нерекомендованным) и закомментировал его

--- work1/Program.cs
+++ work1/Program.cs
@@ -12,15 +12,38 @@ Console.WriteLine(myString[5]);
 Console.WriteLine(myString[6]);
 Console.WriteLine(myString[7]);
 Console.WriteLine();
-int L=3;
+int L = 3;
 Console.WriteLine("Элементы массива с количеством символов <=3: ");
 for (int i = 0; i < myString.Length; i++)
 {
-    int result=myString[i].Length;
-    if(result<=L)
+    int result = myString[i].Length;
+    if (result <= L)
     {
-        
+
         Console.WriteLine($" {myString[i]} ");
     }
-}   
+}
+//Второй (нерекомендованный) вариант решения задачи
+/*string[] myString = { "мир", "one", "Привет", "list", "Строка", "we", "C#", "LINQ" };
+Console.WriteLine("Первоначальный массив");
+Console.WriteLine(myString[0]);
+Console.WriteLine(myString[1]);
+Console.WriteLine(myString[2]);
+Console.WriteLine(myString[3]);
+Console.WriteLine(myString[4]);
+Console.WriteLine(myString[5]);
+Console.WriteLine(myString[6]);
+Console.WriteLine(myString[7]);
+Console.WriteLine();
+List<string> result = new List<string>();
+foreach (string s in myString)
+{
+    if (s.Length <= 3)
+    {
+        result.Add(s);
+    }
+}
+Console.WriteLine("Массив строк, длина которых <= 3 символа:");
+foreach (string s in result)
+    Console.WriteLine(s);*/
 
